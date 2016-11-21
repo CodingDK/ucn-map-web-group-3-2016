334b12952279a30b86520e50639831321692d016
CodingDK <CodingDK@users.noreply.github.com>
2016-11-21 15:53:20 +0100

    Moon layer added to map

--- ucn-map/client/imports/components/mapContainer/mapContainer.js
+++ ucn-map/client/imports/components/mapContainer/mapContainer.js
@@ -6,7 +6,7 @@ import templateUrl from './mapContainer.html';
 
 
 class MapCtrl {
-    constructor($scope, $reactive) {
+    constructor($scope, $reactive, uiGmapGoogleMapApi) {
         'ngInject';
         //$reactive(this).attach($scope);
 
@@ -15,13 +15,15 @@ class MapCtrl {
                 latitude: 57.0269181,
                 longitude: 9.7673928
             },
-            zoom: 11,
+            zoom: 9,
             events: {
                 click: (mapModel, eventName, originalEventArgs) => {
                     this.setLocation(originalEventArgs[0].latLng.lat(), originalEventArgs[0].latLng.lng());
                     $scope.$apply();
                 }
-            }
+            },
+
+            showOverlay: true,
         };
 
         $scope.poly = [
@@ -56,21 +58,59 @@ class MapCtrl {
             }
         ];
 
-        $scope.marker = {
-            options: {
-                draggable: true
-            },
-            events: {}
-        };
+        uiGmapGoogleMapApi.then(function(maps) {
+            // (google is defined)
+
+            $scope.imageMapType = {
+                getTileUrl: function(coord, zoom) {
+                    var normalizedCoord = $scope.getNormalizedCoord(coord, zoom);
+                    if (!normalizedCoord) {
+                        return null;
+                    }
+                    var bound = Math.pow(2, zoom);
+                    return '//mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw' +
+                        '/' + zoom + '/' + normalizedCoord.x + '/' +
+                        (bound - normalizedCoord.y - 1) + '.jpg';
+                },
+                tileSize: new google.maps.Size(256, 256),
+                maxZoom: 9,
+                minZoom: 0,
+                radius: 1738000};
+        });
+
 
         this.setLocation = function(latitude, longitude) {
             $scope.location = {
                 latitude,
                 longitude
             };
-        }
+        };
+
+        // Normalizes the coords that tiles repeat across the x axis (horizontally)
+        // like the standard Google map tiles.
+        $scope.getNormalizedCoord = function(coord, zoom) {
+            var y = coord.y;
+            var x = coord.x;
+
+            // tile range in one direction range is dependent on zoom level
+            // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+            var tileRange = 1 << zoom;
+
+            // don't repeat across y-axis (vertically)
+            if (y < 0 || y >= tileRange) {
+                return null;
+            }
+
+            // repeat across x-axis
+            if (x < 0 || x >= tileRange) {
+                x = (x % tileRange + tileRange) % tileRange;
+            }
+
+            return {x: x, y: y};
+        };
     }
 }
+
 const name = 'mapContainer';
 
 // create a module
