10dac55cd22f4be87500cdc29acf4012ec972e19
CodingDK <CodingDK@users.noreply.github.com>
2016-11-25 11:06:08 +0100

    Added a very simple test

---
+++ ucn-map/server/example.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('#indexOf()', function() {
+    it('should show that test frameworks works', function() {
+        assert.equal(-1, -1);
+    });
+});
+console.log("does it log this message, example.test.js");
+
