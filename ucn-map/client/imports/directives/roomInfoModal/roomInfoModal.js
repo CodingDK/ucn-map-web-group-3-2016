245d357f7c069b18747c51acfcb7049f70547fe4
AnneHamiltonM <annehamilton@Annes-MacBook-Pro.local>
2016-12-06 10:22:45 +0100

    modal ...

---
+++ ucn-map/client/imports/directives/roomInfoModal/roomInfoModal.js
@@ -0,0 +1,47 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import templateUrl from './roomInfoModal.html';
+
+"use strict";
+class TemplateModalCtrl {
+    constructor($uibModal, $state, $timeout) {
+        'ngInject';
+
+        $uibModal.open({
+            templateUrl,
+            controller: function($scope) {
+                $scope.dismiss = function() {
+                    $scope.$dismiss();
+                };
+            },
+            /*resolve: {
+             items: function () {
+             return [1,2,3];
+             }
+             }*/
+        }).result.then(doClosureFn, doDismissFn);
+
+        function doClosureFn() {
+            $timeout(function () {
+                $state.go('selectClassModal');
+            },100);
+        }
+        function doDismissFn() {
+            return doClosureFn();
+        }
+
+    }
+}
+
+const name = 'templateModal';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor
+]).directive(name, function templateModalDirective() {
+    return {
+        //templateUrl,
+        //controllerAs: name,
+        controller: TemplateModalCtrl
+    }
+});
