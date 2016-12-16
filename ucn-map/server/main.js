b268d6c9248717cb30675ab9f2c66b44e3de2ad7
CodingDK <CodingDK@users.noreply.github.com>
2016-12-16 07:08:56 +0100

    disabled all insert and kadira (logging) on production

--- ucn-map/server/main.js
+++ ucn-map/server/main.js
@@ -5,9 +5,11 @@ import {WebUntisCtrl} from './imports/controllers/webUntis';
 
 Meteor.startup(() => {
   // code to run on server at startup
-    Kadira.connect('7NJFMudktKcn2TFRS', '11f6c042-0f83-4c4e-a9a5-4e06ba9c0dd2');
-    //logger.info("test?");
-    insertTestData();
-    let webUntisCtr = new WebUntisCtrl();
-    webUntisCtr.checkWebUntisInformation();
+    if (Meteor.isDevelopment) {
+        Kadira.connect('7NJFMudktKcn2TFRS', '11f6c042-0f83-4c4e-a9a5-4e06ba9c0dd2');
+        //logger.info("test?");
+        insertTestData();
+        let webUntisCtr = new WebUntisCtrl();
+        webUntisCtr.checkWebUntisInformation();
+    }
 });
